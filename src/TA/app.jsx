92e8145a74aa5638ae0e7e831fbe70bb63505725
Pauli <usuario@MacBook-Pro-de-usuario.local>
2024-09-23 20:37:19 -0300

    parte4

---
+++ src/TA/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Counter from './contador.jsx';
+
+function App() {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './TA/App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './TA/app.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
